5f332a67868a6b3cf7198952d1145693c6076955
zhuyingb <yingbing.zhu@hotmail.com>
2025-04-22 16:48:41 -0700

    add singly linked list

---
+++ data-structures/linked-list/MySinglyLinkedList.h
@@ -0,0 +1,16 @@
+//
+// Created by yingbingzhu on 2025/4/21.
+//
+
+#pragma once
+
+#include <cstddef>
+#include <stdexcept>
+
+// A basic singly linked list
+
+template <typename T>
+class MySinglyLinkedList {
+  private:
+
+}
